823f9480f083859152844d1e9f2252281d6742c9
vado21 <vado21>
2021-03-14 19:14:54 -0600

    cambios product list

--- src/admin/ProductsList.js
+++ src/admin/ProductsList.js
@@ -1,4 +1,30 @@
 export default function ProductsList(props) {
+    //BOTON MAS
+    //BOTON DE BASURA
+    //CADA RENGLON UN CHECKBOX
+
+    //Despliegue la n cantidad de productos
+    //ASEGURARME DE CHECKBOX
+    //BOTON DE MAS
+    //BOTON DE BASURA
+    // Barra agregar al boton mas diagonal agregar
+    // id
+    // history.push(`/${sectionName}`) productos/agregar
+    // editar hasta la derecha push productos/id
+    let data = [{id:
+                nombre:
+                descripcion:
+                img:
+                precio:
+                },{ id:
+                    nombre:
+                    descripcion:
+                    img:
+                    precio:},{ id:
+                        nombre:
+                        descripcion:
+                        img:
+                        precio:}]
     return (
         <h3>Hola</h3>
     )
