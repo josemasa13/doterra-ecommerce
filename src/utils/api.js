c89c954d9e0664c66153d6737259d9bbf35e3774
vado21 <vado21>
2021-04-26 22:15:51 -0500

    Eventos terminado

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const url = "https://sm0ccafg1i.execute-api.us-east-1.amazonaws.com/dev"
+const url = "https://doterra-app.herokuapp.com"
 
 export function fetchProduct(productId){
     const endpoint = `${url}/getProduct/${productId}`
@@ -69,6 +69,50 @@ export function deleteProduct(productId){
     const requestOptions = {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json'}
+    };
+    
+    return fetch(endpoint, requestOptions)
+            .then((res) => {
+                return res.json();
+            })
+}
+
+export function fetchEvents(){
+    const endpoint = `${url}/events/getEvents`
+    
+    const requestOptions = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json'}
+    };
+    
+    return fetch(endpoint, requestOptions)
+            .then((res) => {
+                return res.json();
+            })
+}
+
+export function createEvent(EventData){
+    const endpoint = `${url}/events/addEvent`
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(EventData)
+    };
+    
+    return fetch(endpoint, requestOptions)
+            .then((res) => {
+                return res.json();
+            })
+}
+
+export function deleteEvents(eventIds){
+    var endpoint = `${url}/events/deleteEvents`
+    
+    const requestOptions = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(eventIds)
     };
     
     return fetch(endpoint, requestOptions)
